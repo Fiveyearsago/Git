a7228a37fc60f6db354410eaa5838b68e1a82b51
Fiveyearsago <fiveyearsagosr@gmail.com>
2015-10-12 16:51:15 +0800

    Add Adapter

---
+++ app/src/main/java/com/littlecat/git/fragment/DataFragment.java
@@ -0,0 +1,11 @@
+package com.littlecat.git.fragment;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by LittleCat on 2015/10/12.
+ * Email: fiveyearsagosr@gmail.com
+ * GitHub: LittleCat
+ */
+public class DataFragment extends ListFragment {
+}
