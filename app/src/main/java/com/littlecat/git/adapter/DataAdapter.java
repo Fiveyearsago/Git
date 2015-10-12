983b14f380e70e59ce5d6bdc456b8d9862830edf
Fiveyearsago <fiveyearsagosr@gmail.com>
2015-10-12 16:18:39 +0800

    Add Adapter

---
+++ app/src/main/java/com/littlecat/git/adapter/DataAdapter.java
@@ -0,0 +1,32 @@
+package com.littlecat.git.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by LittleCat on 2015/10/12.
+ * Email: fiveyearsagosr@gmail.com
+ * GitHub: LittleCat
+ */
+public class DataAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
